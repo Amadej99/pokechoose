57e1bf83e7b2fa1d61d24cd3381f356b50bb3850
Amadej99 <amadejm@icloud.com>
2022-11-05 20:58:30 +0100

    initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style='light' />
+      <View className='flex min-h-screen items-center justify-center bg-black'>
+        <Text className='text-white'>
+          Open up App.js to start working on your app!
+        </Text>
+      </View>
+    </>
+  );
+}
