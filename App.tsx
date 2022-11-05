fffbc6e5567a0f6fef422301dc0d49d3e74673a8
Amadej99 <amadejm@icloud.com>
2022-11-05 21:03:18 +0100

    doublequotes prettier

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
     <>
-      <StatusBar style='light' />
-      <View className='flex min-h-screen items-center justify-center bg-black'>
-        <Text className='text-white'>
+      <StatusBar style="light" />
+      <View className="flex min-h-screen items-center justify-center bg-black">
+        <Text className="text-white">
           Open up App.js to start working on your app!
         </Text>
       </View>
