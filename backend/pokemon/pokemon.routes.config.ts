9a492ca459a953bc24fec73f616daf321ebcb4eb
Amadej <amadejm@icloud.com>
2022-11-12 16:06:20 +0100

    routes WIP

--- backend/pokemon/pokemon.routes.config.ts
+++ backend/pokemon/pokemon.routes.config.ts
@@ -1,4 +1,11 @@
 import express from "express";
+import { prisma } from "../prisma/database";
+
+type Pokemon = {
+  id: number;
+  name: string;
+  image: string;
+};
 export class PokemonRoutes {
   app: express.Application;
   name: string;
@@ -14,17 +21,81 @@ export class PokemonRoutes {
   configureRoutes(): express.Application {
     this.app
       .route(`/pokemon/:id`)
-      .get((req: express.Request, res: express.Response) => {
-        res.status(200).send(`Return pokemon.`);
-      })
-      .post((req: express.Request, res: express.Response) => {
-        res.status(200).send(`Create pokemon`);
-      })
+      // .get(async (req: express.Request, res: express.Response) => {
+      //   const pokemon = await prisma.pokemon.findUnique({
+      //     where: {
+      //       id: Number(req.params.id),
+      //     },
+      //   });
+      //   if (pokemon) {
+      //     res.status(200).send(pokemon);
+      //   } else {
+      //     res.status(404).send("Pokemon not found");
+      //   }
+      // })
       .put((req: express.Request, res: express.Response) => {
         res.status(200).send(`Update pokemon`);
       })
       .delete((req: express.Request, res: express.Response) => {
         res.status(200).send(`Delete pokemon`);
+      });
+
+    this.app
+      .route(`/pokemon/:id/upvote`)
+      .post(async (req: express.Request, res: express.Response) => {
+        const pokemon = await prisma.pokemon.update({
+          where: {
+            id: Number(req.params.id),
+          },
+          data: {
+            votedFor: {
+              increment: 1,
+            },
+          },
+        });
+
+        res.status(200).send(pokemon.name + " has been upvoted!");
+      });
+
+    this.app
+      .route(`/pokemon/:id/downvote`)
+      .post(async (req: express.Request, res: express.Response) => {
+        const pokemon = await prisma.pokemon.update({
+          where: {
+            id: Number(req.params.id),
+          },
+          data: {
+            votedAgainst: {
+              increment: 1,
+            },
+          },
+        });
+
+        res.status(200).send(pokemon.name + " has been downvoted!");
+      });
+
+    this.app
+      .route(`/pokemonCreate`)
+      .post(async (req: express.Request, res: express.Response) => {
+        req.body.pokemonList.forEach(async (pokemon: Pokemon) => {
+          await prisma.pokemon.upsert({
+            where: {
+              id: pokemon.id,
+            },
+            update: {},
+            create: {
+              id: Number(pokemon.id),
+              name: pokemon.name,
+              image: pokemon.image,
+            },
+            select: {
+              id: true,
+              name: true,
+              image: true,
+            },
+          });
+        });
+        res.status(200).send("Pokemon created");
       });
 
     this.app
