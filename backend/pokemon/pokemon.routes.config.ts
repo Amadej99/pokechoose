7ada775f0e35af8e259a96d8ed1e7cf58769bf97
Amadej99 <amadejm@icloud.com>
2022-12-31 15:46:27 +0100

    removed unnecessary routes

--- backend/pokemon/pokemon.routes.config.ts
+++ backend/pokemon/pokemon.routes.config.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import { prisma } from "../prisma/database";
+import express from 'express';
+import { prisma } from '../prisma/database';
 
 type Pokemon = {
   id: number;
@@ -12,7 +12,7 @@ export class PokemonRoutes {
 
   constructor(app: express.Application) {
     this.app = app;
-    this.name = "PokemonRoutes";
+    this.name = 'PokemonRoutes';
     this.configureRoutes();
   }
   getName() {
@@ -21,18 +21,6 @@ export class PokemonRoutes {
   configureRoutes(): express.Application {
     this.app
       .route(`/pokemon/:id`)
-      // .get(async (req: express.Request, res: express.Response) => {
-      //   const pokemon = await prisma.pokemon.findUnique({
-      //     where: {
-      //       id: Number(req.params.id),
-      //     },
-      //   });
-      //   if (pokemon) {
-      //     res.status(200).send(pokemon);
-      //   } else {
-      //     res.status(404).send("Pokemon not found");
-      //   }
-      // })
       .put((req: express.Request, res: express.Response) => {
         res.status(200).send(`Update pokemon`);
       })
@@ -65,7 +53,7 @@ export class PokemonRoutes {
           },
         });
 
-        res.status(200).send(pokemon.name + " has been upvoted!");
+        res.status(200).send(pokemon.name + ' has been upvoted!');
       });
 
     this.app
@@ -82,31 +70,7 @@ export class PokemonRoutes {
           },
         });
 
-        res.status(200).send(pokemon.name + " has been downvoted!");
-      });
-
-    this.app
-      .route(`/pokemonCreate`)
-      .post(async (req: express.Request, res: express.Response) => {
-        req.body.pokemonList.forEach(async (pokemon: Pokemon) => {
-          await prisma.pokemon.upsert({
-            where: {
-              id: pokemon.id,
-            },
-            update: {},
-            create: {
-              id: Number(pokemon.id),
-              name: pokemon.name,
-              image: pokemon.image,
-            },
-            select: {
-              id: true,
-              name: true,
-              image: true,
-            },
-          });
-        });
-        res.status(200).send("Pokemon created");
+        res.status(200).send(pokemon.name + ' has been downvoted!');
       });
 
     this.app
@@ -131,10 +95,17 @@ export class PokemonRoutes {
         });
 
         pokemon.sort((a, b) => {
-          return (
+          if (
             b.votedFor / (b.votedFor + b.votedAgainst) -
-            a.votedFor / (a.votedFor + a.votedAgainst)
-          );
+              a.votedFor / (a.votedFor + a.votedAgainst) ==
+            0
+          )
+            return b.votedFor - a.votedFor;
+          else
+            return (
+              b.votedFor / (b.votedFor + b.votedAgainst) -
+              a.votedFor / (a.votedFor + a.votedAgainst)
+            );
         });
 
         res.status(200).send(pokemon);
