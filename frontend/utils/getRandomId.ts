3e11392e011ef55e97432aebbd76bb7f5728abac
Amadej99 <amadejm@icloud.com>
2023-01-05 00:51:08 +0100

    sort before checking for duplicates/returning

--- frontend/utils/getRandomId.ts
+++ frontend/utils/getRandomId.ts
@@ -2,9 +2,12 @@ export function getRandomPair(alreadyChosen: Number[][]) {
   const firstId = Math.floor(Math.random() * 900);
   const secondId = Math.floor(Math.random() * 900);
 
-  if (alreadyChosen.includes([firstId, secondId]) || firstId === secondId) {
+  if (
+    alreadyChosen.includes([firstId, secondId].sort((a, b) => a - b)) ||
+    firstId === secondId
+  ) {
     return getRandomPair(alreadyChosen);
   }
 
-  return [firstId, secondId];
+  return [firstId, secondId].sort((a, b) => a - b);
 }
